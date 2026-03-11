cf3a21c526c89a893631c893cbc3875bf1a8d3de
Vermzzyy <cpt.woof16@gmail.com>
2026-03-11 21:53:12 +0800

    Initial CreatorHub backend

---
+++ src/main/java/com/creatorshub/creatorshub/CreatorshubApplication.java
@@ -0,0 +1,13 @@
+package com.creatorshub.creatorshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorshubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creatorshub/creatorshub/CreatorshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creatorshub.creatorshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
